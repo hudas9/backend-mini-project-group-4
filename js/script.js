c023346f37295a5c595c468c0a2ad7dba1d76f9c
Ahmad Huda Salam <ahmadhudasalam9@gmail.com>
2024-03-27 13:34:06 +0700

    call menu from data.json, add animation for close navbar-menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $('#bars').click(function () {
-  $('.navbar-menu').addClass('active');
+  $('.navbar-menu').addClass('show');
 });
 
 $('#xmark').click(function () {
-  $('.navbar-menu').removeClass('active');
+  $('.navbar-menu').removeClass('show');
+  $('.navbar-menu').addClass('hide');
+  setTimeout(function () {
+    $('.navbar-menu').removeClass('hide');
+  }, 500);
 });
